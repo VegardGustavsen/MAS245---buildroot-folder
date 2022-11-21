539d9b69c29d9cafcb5383d2ad225d514b1f6628
VegardGustavsen <114565192+VegardGustavsen@users.noreply.github.com>
2022-11-21 19:55:03 +0100

    Added example code
    Added example code used in project

---
+++ project_files/examples_used/teensy36_oled128x64_demo/teensy36_oled128x64_demo.ino
@@ -0,0 +1,289 @@
+
+#include <FlexCAN.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
+
+Adafruit_MPU6050 mpu;
+
+#include <Fonts/FreeMono9pt7b.h>
+const int JOY_LEFT = 18;
+const int JOY_RIGHT = 17;
+const int JOY_CLICK =19;
+const int JOY_UP  = 22;
+const int JOY_DOWN = 23;
+ 
+#define OLED_DC     6
+#define OLED_CS     10
+#define OLED_RESET  5
+Adafruit_SSD1306 display(OLED_DC, OLED_RESET, OLED_CS);
+#define SSD1306_128_64;
+
+#if (SSD1306_LCDHEIGHT != 64)
+#error("Height incorrect, please fix Adafruit_SSD1306.h!");
+#endif
+
+static CAN_message_t msg,rxmsg;
+volatile uint32_t count = 0;
+IntervalTimer TX_timer;
+String CANStr(""); 
+volatile uint32_t can_msg_count = 0;
+
+
+
+void setup(void){
+ 
+ //MPU
+ Serial.begin(115200);
+  while (!Serial)
+    delay(10); // will pause Zero, Leonardo, etc until serial console opens
+
+  Serial.println("Adafruit MPU6050 test!");
+
+  // Try to initialize!
+  if (!mpu.begin()) {
+    Serial.println("Failed to find MPU6050 chip");
+    while (1) {
+      delay(10);
+    }
+  }
+  Serial.println("MPU6050 Found!");
+
+  mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
+  Serial.print("Accelerometer range set to: ");
+  switch (mpu.getAccelerometerRange()) {
+  case MPU6050_RANGE_2_G:
+    Serial.println("+-2G");
+    break;
+  case MPU6050_RANGE_4_G:
+    Serial.println("+-4G");
+    break;
+  case MPU6050_RANGE_8_G:
+    Serial.println("+-8G");
+    break;
+  case MPU6050_RANGE_16_G:
+    Serial.println("+-16G");
+    break;
+  }
+  mpu.setGyroRange(MPU6050_RANGE_500_DEG);
+  Serial.print("Gyro range set to: ");
+  switch (mpu.getGyroRange()) {
+  case MPU6050_RANGE_250_DEG:
+    Serial.println("+- 250 deg/s");
+    break;
+  case MPU6050_RANGE_500_DEG:
+    Serial.println("+- 500 deg/s");
+    break;
+  case MPU6050_RANGE_1000_DEG:
+    Serial.println("+- 1000 deg/s");
+    break;
+  case MPU6050_RANGE_2000_DEG:
+    Serial.println("+- 2000 deg/s");
+    break;
+  }
+
+  mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
+  Serial.print("Filter bandwidth set to: ");
+  switch (mpu.getFilterBandwidth()) {
+  case MPU6050_BAND_260_HZ:
+    Serial.println("260 Hz");
+    break;
+  case MPU6050_BAND_184_HZ:
+    Serial.println("184 Hz");
+    break;
+  case MPU6050_BAND_94_HZ:
+    Serial.println("94 Hz");
+    break;
+  case MPU6050_BAND_44_HZ:
+    Serial.println("44 Hz");
+    break;
+  case MPU6050_BAND_21_HZ:
+    Serial.println("21 Hz");
+    break;
+  case MPU6050_BAND_10_HZ:
+    Serial.println("10 Hz");
+    break;
+  case MPU6050_BAND_5_HZ:
+    Serial.println("5 Hz");
+    break;
+  }
+
+  Serial.println("");
+  delay(100);
+  //MPU
+
+  Can0.begin(500000); //set speed here. 
+  Can1.begin(500000); //set speed here.  
+  
+  pinMode(JOY_LEFT, INPUT_PULLUP);
+  pinMode(JOY_RIGHT, INPUT_PULLUP);
+  pinMode(JOY_CLICK, INPUT_PULLUP);
+  pinMode(JOY_UP, INPUT_PULLUP);
+  pinMode(JOY_DOWN, INPUT_PULLUP); 
+  
+  // by default, we'll generate the high voltage from the 3.3v line internally! (neat!)
+  display.begin(SSD1306_SWITCHCAPVCC);
+  // Clear the buffer.
+  display.clearDisplay();
+  
+  delay(1000);
+  Serial.println(F("CAN Bus Tx test"));
+
+  display.setTextSize(0);
+
+  
+  
+  delay(1000);
+  
+  display.clearDisplay();
+  
+  msg.buf[0] = 1;
+  msg.buf[1] = 2;
+  msg.buf[2] = 0;
+  msg.buf[3] = 0;
+  msg.buf[4] = 0;
+  msg.buf[5] = 0;
+  msg.buf[6] = 0;
+  msg.len = 8;
+  msg.id = 0x7DF;
+  msg.flags.extended = 0; 
+  msg.flags.remote = 0;
+//  msg.timeout = 500;
+  TX_timer.begin(tx_CAN, 1000000);    /* Start interrutp timer */
+  
+}
+
+/* From Timer Interrupt */
+void tx_CAN(void)
+{
+  msg.len = 8;
+  msg.id = 0x7DF;
+  msg.buf[0] = 0x02;
+  msg.buf[1] = 0x01;
+  msg.buf[2] = 0x0c;
+  msg.buf[3] = 0;
+  msg.buf[4] = 0;
+  msg.buf[5] = 0;
+  msg.buf[6] = 0;
+  msg.buf[7] = 0;
+  /*
+  msg.buf[4] = can_msg_count >> 24;
+  msg.buf[5] = can_msg_count >> 16;
+  msg.buf[6] = can_msg_count >> 8;
+  msg.buf[7] = can_msg_count;
+  */
+  Can0.write(msg);
+  Can1.write(msg);
+  can_msg_count++;
+}
+
+void loop() {
+ 
+ 
+   
+  while(Can0.read(rxmsg))  { 
+     String CANStr(""); 
+     for (int i=0; i < 8; i++) {     
+
+         CANStr += String(rxmsg.buf[i],HEX);
+         CANStr += (" ") ;
+     }
+     Serial.print(rxmsg.id,HEX); 
+     Serial.print(' '); 
+     Serial.print(rxmsg.len,HEX); 
+     Serial.print(' ');
+     Serial.println(CANStr);  
+     
+      //PRINTING DISPLAY IN LOOP
+    
+  
+
+
+  display.clearDisplay();
+
+  display.setTextSize(0);
+  display.setTextColor(WHITE);
+
+  display.setCursor(15,3);
+  display.println("MAS245 - Gruppe 4");
+
+  display.drawLine(8, 12, 120, 12, WHITE);
+  display.drawRoundRect(1, 1, 127, 63, 4, WHITE);
+    
+  display.setCursor(8, 16);
+  display.println("CAN-statistikk");
+  
+  display.setCursor(8, 22);
+  display.println("--------------");
+  
+  display.setCursor(8, 27);
+  display.println("Antall mottatt: ");
+  
+  display.setCursor(8, 35);
+  display.println("Mottok sist ID: ");
+  
+  display.setCursor(8, 42);
+  display.println("--------------");
+
+  display.setCursor(8, 48);
+  display.println("IMU-maling z: ");
+
+//Antall mottatt:
+  display.setCursor(99, 27);
+  display.println(can_msg_count);
+
+//Mottok sist ID:
+  display.setCursor(99, 35);
+  display.println(rxmsg.id,HEX);
+
+//IMU:
+
+sensors_event_t a, g, temp;
+  mpu.getEvent(&a, &g, &temp);
+
+  /* Print out the values */
+  Serial.print("Acceleration X: ");
+  Serial.print(a.acceleration.x);
+  Serial.print(", Y: ");
+  Serial.print(a.acceleration.y);
+  Serial.print(", Z: ");
+  Serial.print(a.acceleration.z);
+  Serial.println(" m/s^2");
+
+  Serial.print("Rotation X: ");
+  Serial.print(g.gyro.x);
+  Serial.print(", Y: ");
+  Serial.print(g.gyro.y);
+  Serial.print(", Z: ");
+  Serial.print(g.gyro.z);
+  Serial.println(" rad/s");
+
+  Serial.print("Temperature: ");
+  Serial.print(temp.temperature);
+  Serial.println(" degC");
+
+  Serial.println("");
+  delay(500);
+
+
+   //display.setCursor(99, 48);
+    //display.println(a.acceleration.z);
+
+
+
+  /*Serial.print(", Z: ");
+  Serial.print(a.acceleration.z);
+  Serial.println(" m/s^2");
+*/
+
+
+  display.display();
+  }
+     
+}
+
+
+
